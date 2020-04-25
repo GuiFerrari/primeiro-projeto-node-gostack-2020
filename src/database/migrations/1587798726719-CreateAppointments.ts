745e389beade5dadbf7115a14ba8ed9c5d9c4b8e
GuiFerrari <guiferrari10@hotmail.com>
2020-04-25 04:50:46 -0300

    Added connection with database

--- src/database/migrations/1587798726719-CreateAppointments.ts
+++ src/database/migrations/1587798726719-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587798726719
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
